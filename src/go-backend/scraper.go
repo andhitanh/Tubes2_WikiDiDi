0332242c31d32e8454a650ab302c7198d9d2f19d
Melati Anggraini <112107340+mlatia@users.noreply.github.com>
2024-04-20 19:41:39 +0700

    fix filter scraping

--- src/go-backend/scraper.go
+++ src/go-backend/scraper.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json" // mengelola data JSON
 	"fmt"           // format I/O
 	"log"           // pesan kesalahan
+	"regexp"        // regex
+	// "strings"
 	"time"
 
 	"github.com/gocolly/colly" // framework scraping
@@ -23,37 +25,109 @@ func timer(name string) func() {
 	}
 }
 
-func main() {
-	defer timer("main")()
-	c := colly.NewCollector() // inisialisasi kolektor mode scraping sinkronus(berturu-turut) -- konfigurasi awal
+func scrapeLinks(url string) ([]Item, error) {
+	c := colly.NewCollector()
 
-	items := []Item{}
+	var items []Item
 
-	// menentukan callback yang akan dipanggil ketika ada elemen HTML <a>
-	// mengsmbil judul dan url setiap tautan
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		title := e.Text
-		if title != "" {
-			link := e.Attr("href")
+	c.OnHTML("#mw-content-text a", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		if isValidWikiURL(link) == false {
 			item := Item{
-				Title: title,
+				Title: e.Text,
 				URL:   e.Request.AbsoluteURL(link),
 			}
 			items = append(items, item)
 		}
 	})
 
-	// menentukan callback yang akan dipanggil ketika permintaan selesai sebelum permintaan HTTP dilakukan
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String()) // cetak URL yang dikunjungi
+		fmt.Println("Visiting", r.URL.String())
 	})
 
-	c.Visit("https://id.wikipedia.org/wiki/Melati")
+	err := c.Visit(url)
+	if err != nil {
+		return nil, err
+	}
 
-	data, err := json.MarshalIndent(items, "", " ") // mengonversi data ke JSON
+	return items, nil
+}
+
+func isValidWikiURL(url string) bool {
+	strings := regexp.MustCompile(`\Ahttps://id\.wikipedia\.org/wiki/[^#:/\.]+\z`)
+	return strings.MatchString(url)
+	// if strings.HasPrefix(url, "https://id.wikipedia.org/wiki/") {
+	// 	// Check if the URL contains /wiki/ to ensure it's a Wikipedia link
+	// 	if strings.Contains(url, "id.wikipedia.org/wiki/") {
+	// 		if !strings.ContainsAny(url, "#:/.") {
+	// 			return true
+	// 		}
+	// 	}
+	// }
+	// return false
+	// if !strings.ContainsAny(url, "#:/.%") {
+	// 	if !Contains(urls, url) && url != Convert(strings.url) {
+	// 		return true
+	// 	}
+	// }
+}
+
+func printTitles(links []Item) {
+	for _, link := range links {
+		fmt.Println(link.Title)
+	}
+}
+
+func main() {
+	defer timer("main")()
+
+	url := "https://id.wikipedia.org/wiki/Melati"
+	items, err := scrapeLinks(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// printTitles(items)
+
+	data, err := json.MarshalIndent(items, "", " ")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(string(data))
 }
+
+// func main() {
+// 	defer timer("main")()
+// 	c := colly.NewCollector() // inisialisasi kolektor mode scraping sinkronus(berturu-turut) -- konfigurasi awal
+
+// 	items := []Item{}
+
+// 	// menentukan callback yang akan dipanggil ketika ada elemen HTML <a>
+// 	// mengsmbil judul dan url setiap tautan
+// 	c.OnHTML("a", func(e *colly.HTMLElement) {
+// 		title := e.Text
+// 		if title != "" {
+// 			link := e.Attr("href")
+// 			item := Item{
+// 				Title: title,
+// 				URL:   e.Request.AbsoluteURL(link),
+// 			}
+// 			items = append(items, item)
+// 		}
+// 	})
+
+// 	// menentukan callback yang akan dipanggil ketika permintaan selesai sebelum permintaan HTTP dilakukan
+// 	c.OnRequest(func(r *colly.Request) {
+// 		fmt.Println("Visiting", r.URL.String()) // cetak URL yang dikunjungi
+// 	})
+
+// 	c.Visit("https://id.wikipedia.org/wiki/Melati")
+
+// 	data, err := json.MarshalIndent(items, "", " ") // mengonversi data ke JSON
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	fmt.Println(string(data))
+// }
